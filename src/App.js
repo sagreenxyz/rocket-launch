51949b6ed30a0a49c5591d5828d91b31193fd586
Scott Green <sagreenxyz@gmail.com>
2022-03-23 20:52:05 -0500

    5. stub for RocketLauncher component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 
+function RocketLauncher() {
+  return (
+    <div>
+      RocketLauncher
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
       <main>
         <h1>Learning Cleanup!</h1>
+        <RocketLauncher />
       </main>
     </div>
   );
