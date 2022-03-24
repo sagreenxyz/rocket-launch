9e9c11c168df52f676704c0f012314562783b65a
Scott Green <sagreenxyz@gmail.com>
2022-03-23 20:50:49 -0500

    4. baseline at origination of learning activity

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <main>
+        <h1>Learning Cleanup!</h1>
+      </main>
     </div>
   );
 }
